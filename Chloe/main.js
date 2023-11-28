453df5b5317a168b1aac83ed7f85a737273418fe
Qiwenzhao7 <145478657+Qiwenzhao7@users.noreply.github.com>
2023-11-27 22:55:49 -0500

    Add files via upload

---
+++ Chloe/main.js
@@ -0,0 +1,15 @@
+
+let blobs = document.querySelectorAll(".blob");
+  let container = document.querySelector(".wrapper");
+  // place our blobs randomly
+
+container.addEventListener("click", blobDrop)
+
+function blobDrop() {
+    let newBlob = document.createElement("div");
+    newBlob.classList.add("blob");
+    container.appendChild(newBlob);
+    newBlob.style.right = event.clientX + "px";
+    newBlob.style.top = event.clientY + "px";
+    console.log(event, event.clientX);
+}
